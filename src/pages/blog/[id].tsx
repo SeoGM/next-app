4a648cdd7e3694b831b5bd687aa9ac1ac7bcedbd
서광명 <auddlzz000@gmail.com>
2023-03-08 04:54:55 +0000

    Update index.mjs and [id].tsx

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,40 +3,38 @@ import { GetStaticProps, GetStaticPaths } from "next";
 import { Post as IPost } from "@/interface/Post";
 import formatDate from "@/util/formatDate";
 
-export default function Post({ postData }: { postData: IPost }) {
-  const { title, createdAt, body } = postData;
-  return (
-    <Container>
-      <div className="prose w-full max-w-full">
-        {/* <h1 className="text-sky-700">{post.title}</h1> */}
-        <h1 className="text-sky-700">{title}</h1>
-        <div className="text-sm font-light mb-5">{formatDate(createdAt)}</div>
-        <div dangerouslySetInnerHTML={{ __html: body }} />
-        {/* <MDXComponent /> */}
-      </div>
-    </Container>
-  );
+export default function Post() {
+  return <div></div>;
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${params?.id}`);
-  const postData: IPost = await res.json();
-  console.log("postData", postData);
-  return {
-    props: {
-      postData,
-    },
-  };
-};
+// export default function Post({ postData }: { postData: IPost }) {
+//   const { title, createdAt, body } = postData;
+//   return (
+//     <Container>
+//       <div className="prose w-full max-w-full">
+//         <h1 className="text-sky-700">{title}</h1>
+//         <div className="text-sm font-light mb-5">{formatDate(createdAt)}</div>
+//         <div dangerouslySetInnerHTML={{ __html: body }} />
+//       </div>
+//     </Container>
+//   );
+// }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [
-      // String variant:
-      "/blog/post",
-      // Object variant:
-      { params: { id: "1" } },
-    ],
-    fallback: true,
-  };
-};
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const res = await fetch(`http://localhost:3000/api/posts/${params?.id}`);
+//   const posts: IPost = await res.json();
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// };
+
+// export const GetStaticPathsContext: GetStaticPaths = async () => {
+//   const res = await fetch(`http://localhost:3000/api/posts`);
+//   const posts = await res.json();
+//   const paths = posts.map((post: IPost) => ({
+//     params: { id: post.id },
+//   }));
+//   return { paths, fallback: false };
+// };
