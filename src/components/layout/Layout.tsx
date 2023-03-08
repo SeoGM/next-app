2df0c1510a6a1c2ac9e6752370ec38a220ba7264
서광명 <auddlzz000@gmail.com>
2023-03-08 02:04:05 +0000

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import { metadata } from "@/data/metadata";
 import Head from "next/head";
 import { PropsWithChildren } from "react";
 import Footer from "./Footer";
@@ -8,8 +7,16 @@ export default function Layout(props: PropsWithChildren) {
   return (
     <>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content="BLINGLOG" />
+        <title>서광명 | 5년차 프론트엔드 개발자 포트폴리오</title>
+        <meta
+          name="description"
+          content="5년차 프론트엔드 개발자 서광명의 포트폴리오입니다."
+        />
+        <meta
+          name="keywords"
+          content="프론트엔드, 개발자, 웹퍼블리셔, 웹사이트 만들기"
+        />
+        <meta name="author" content="서광명" />
       </Head>
       <Header />
       <main>{props.children}</main>
